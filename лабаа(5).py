f8965b233d6e949b18ac260ee69cb5c8992d537d
Maryan27 <151569278+Maryan27@users.noreply.github.com>
2023-11-26 19:44:14 +0200

    add 5 laba

---
+++ лабаа(5).py
@@ -0,0 +1,53 @@
+class Sneakers:
+    def __init__(self, brand, size, color, price, quantity, material, numberOfSales):
+        self.brand = brand
+        self.size = size
+        self.color = color
+        self.price = price
+        self.quantity = quantity
+        self.material = material
+        self.numberOfSales = numberOfSales
+
+    
+class SportShoesStore:
+    def __init__(self):
+        self.inventory = []
+
+    def add_sneakers(self, sneakers):
+        self.inventory.append(sneakers)
+
+    def sort_by_price(self):
+        self.inventory.sort(key=lambda x: x.price, reverse=True)
+
+    def sort_by_quantity(self):
+        self.inventory.sort(key=lambda x: x.quantity, reverse=True)
+
+    def top_sneakers(self, n=5):
+        sorted_inventory = sorted(self.inventory, key=lambda x: x.numberOfSales, reverse=True)
+        return sorted_inventory[:n]
+
+    
+def main():
+
+    sneaker1 = Sneakers("Nike", 45, "Чорний", 130, 60, "Шкіра", 110)
+    sneaker2 = Sneakers("Adidas", 44, "Білий", 90, 40, "Фетр", 90)
+
+    store = SportShoesStore()
+    store.add_sneakers(sneaker1)
+    store.add_sneakers(sneaker2)
+
+    print("Склад для сортування:")
+    for sneaker in store.inventory:
+        print(f"{sneaker.brand} - {sneaker.price}")
+
+    store.sort_by_price()
+    print("\nСклад після сортування за ціною:")
+    for sneaker in store.inventory:
+        print(f"{sneaker.brand} - {sneaker.price}")
+
+    top_sneakers = store.top_sneakers()
+    print("\nТоп 5 за популярністю:")
+    for sneaker in top_sneakers:
+
+if __name__ == __main__
+
